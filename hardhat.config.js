1a1af17c2cbc8b69cb82fe8ba27d2915772f3ffe
visionEye0 <pranavsayshii@gmail.com>
2025-07-23 19:03:32 +0000

    Added hardhat configuration for sepolia network

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,10 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.SEPOLIA_WALLET_PRIVATE_KEY],
+    },
+  },
 };
